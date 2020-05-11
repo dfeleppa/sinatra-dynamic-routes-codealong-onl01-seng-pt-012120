2c0a19a7f05a9ac7a2336c4b3b173db187bbb364
Daniel J Feleppa <dfeleppa@gmail.com>
2020-05-11 02:26:35 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,7 +19,7 @@ class App < Sinatra::Base
     "Goodbye, #{@user_name}."
   end
 
-   get "/:operation/:number1/:number2" do
+   get "/multiply/:number1/:number2" do
     @number_1 = params[:number1].to_i
     @number_2 = params[:number2].to_i
     params[:operation] == "multiply"

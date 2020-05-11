1c5eecc780a7fa00f792c4f7405aa40f44cf6d7a
Daniel J Feleppa <dfeleppa@gmail.com>
2020-05-11 02:21:22 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,7 +20,7 @@ class App < Sinatra::Base
   end
 
   get "/multiply/:number1/:number2" do
-    @resuls = "#{params[:num1].to_i*params[:num2].to_i}"
+    @result = "#{params[:num1].to_i*params[:num2].to_i}"
     "#{result}"
 
 end
